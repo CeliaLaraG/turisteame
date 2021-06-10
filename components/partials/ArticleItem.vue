f63b39419d0c9a75c499202835df45df1b35081b
CeliaLaraG <celia.lara.garcia@gmail.com>
2021-06-10 16:46:46 +0200

    frontend prueba

--- components/partials/ArticleItem.vue
+++ components/partials/ArticleItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="column is-flex">
+  <div class="column is-multiline is-flex">
     <div class="card m-5">
       <div class="card-image">
         <figure class="image is-4by3">
@@ -25,18 +25,7 @@
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ipsa voluptatem atque eligendi ab. Alias officiis vel incidunt reprehenderit dolore explicabo blanditiis voluptatum saepe, repellat eum exercitationem esse iste fuga similique architecto distinctio placeat nulla assumenda doloremque quos est iure!</p>
      </div>
     </div>
-   <div class="card m-5">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-        </figure>
-      </div>
-      <div class="card-content">
-     <h1 class="title">Ejercicio de prueba</h1>
-     <p>esto va a ser una entradilla</p>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ipsa voluptatem atque eligendi ab. Alias officiis vel incidunt reprehenderit dolore explicabo blanditiis voluptatum saepe, repellat eum exercitationem esse iste fuga similique architecto distinctio placeat nulla assumenda doloremque quos est iure!</p>
-     </div>
-    </div>
+  
 </div>
 
 </template>
